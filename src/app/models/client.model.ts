4a1643a99f4a57a576eb29a53603e7d500f5f9ba
iagop <iagopuzer06@hotmail.com>
2025-03-05 20:49:23 -0300

    Implement clients feature with dynamic client listing

---
+++ src/app/models/client.model.ts
@@ -0,0 +1,8 @@
+export interface Client {
+  id: number;
+  name: string;
+  companyValuation: number;
+  salary: number;
+  createdAt: string;
+  updatedAt: string;
+}
--- src/app/pages/clients/clients.component.ts
+++ src/app/pages/clients/clients.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CardComponent } from '../../components/card/card.component';
+import { UsersService } from '../../services/users.service';
+import { Client } from '../../models/client.model';
 
 @Component({
   selector: 'app-clients',
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './clients.component.html',
-  styleUrl: './clients.component.css'
+  styleUrl: './clients.component.css',
 })
-export class ClientsComponent {
+export class ClientsComponent implements OnInit {
+  clients: Client[] = [];
+  currentPage: number = 0;
+  totalPages: number = 0;
 
+  constructor(private usersService: UsersService) {}
+
+  async ngOnInit() {
+    try {
+      const response = await this.usersService.getUsers();
+      this.clients = response.clients;
+      this.currentPage = response.currentPage;
+      this.totalPages = response.totalPages;
+    } catch (error) {
+      console.error('Error fetching users in component:', error);
+    }
+  }
 }
